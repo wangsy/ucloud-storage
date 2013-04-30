311822db797539a3e43a691bbf02777eea64a7f1
KunHa <potato9@gmail.com>
2013-04-30 11:29:53 +0900

    yields response

--- lib/ucloud_storage/ucloud_storage.rb
+++ lib/ucloud_storage/ucloud_storage.rb
@@ -6,39 +6,42 @@ module UcloudStorage
 	class NotAuthorized < StandardError; end
 
 	class UcloudStorage
-		attr_accessor :user, :pass, :storage_url, :auth_token, :response_code
+		attr_accessor :user, :pass, :storage_url, :auth_token
 
 		def authorize
-			response = HTTParty.get("https://api.ucloudbiz.olleh.com/storage/v1/auth/", 
-				headers: { "X-Storage-User" => user, "X-Storage-Pass" => pass})	
+			response = HTTParty.get("https://api.ucloudbiz.olleh.com/storage/v1/auth/",
+														 	headers: { "X-Storage-User" => user,
+														 						 "X-Storage-Pass" => pass })
+
+			yield response if block_given?
+
 			case response.code
-			when 200 then
+			when 200
 				self.storage_url = response.headers["X-Storage-Url"]
 				self.auth_token = response.headers["X-Auth-Token"]
 				true
-			when 401 then
-				false
-			else
-				raise TotalyWrongException
+			when 401 then false
+			else raise TotalyWrongException
 			end
 		end
 
 		def upload(file_path, box_name, destination)
 			raise NotAuthorized if storage_url.nil?
-			file = File.new(file_path)		
-			contenttype = `file --mime-type #{file_path}`.split(": ").last
-			response = HTTParty.put(storage_url+ "/#{box_name}/#{destination}", 
-				headers: {
-					"X-Auth-Token" => auth_token,
-					"Content-Type" => contenttype,
-					"Content-Length" => file.size.to_s },
-				body: file.read)
-			self.response_code = response.code
-			case self.response_code
-			when 201 then
-				true
-			else
-				false
+
+			file = File.new(file_path)
+			content_type = `file --mime-type #{file_path}`.split(": ").last
+			response = HTTParty.put(storage_url+ "/#{box_name}/#{destination}",
+															headers: {
+																"X-Auth-Token" => auth_token,
+																"Content-Type" => content_type,
+																"Content-Length" => file.size.to_s },
+															body: file.read)
+
+			yield response if block_given?
+
+			case response.code
+			when 201 then true
+			else false
 			end
 		end
 	end
