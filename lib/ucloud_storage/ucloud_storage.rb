a794c6ded36f1c3ff2d9a03ccad870c3afdbf226
Sooyong Wang <wangsy@wangsy.com>
2013-06-12 18:39:39 +0900

    resolve bug about authorization fail when uploading

--- lib/ucloud_storage/ucloud_storage.rb
+++ lib/ucloud_storage/ucloud_storage.rb
@@ -2,60 +2,65 @@
 require 'httparty'
 
 module UcloudStorage
-	class TotalyWrongException < StandardError; end
-	class NotAuthorized < StandardError; end
+  class TotalyWrongException < StandardError; end
+  class NotAuthorized < StandardError; end
 
-	class UcloudStorage
-		attr_accessor :user, :pass, :storage_url, :auth_token
+  class UcloudStorage
+    attr_accessor :user, :pass, :storage_url, :auth_token
 
-		def initialize(options={})
-			@user = options.fetch(:user) { Configuration.user }
-			@pass = options.fetch(:pass) { Configuration.pass }
-		end
+    def initialize(options={})
+      @user = options.fetch(:user) { Configuration.user }
+      @pass = options.fetch(:pass) { Configuration.pass }
+    end
 
-		def authorize
-			response = HTTParty.get("https://api.ucloudbiz.olleh.com/storage/v1/auth/",
-														 	headers: { "X-Storage-User" => user,
-														 						 "X-Storage-Pass" => pass })
+    def authorize
+      response = HTTParty.get("https://api.ucloudbiz.olleh.com/storage/v1/auth/",
+                              headers: { "X-Storage-User" => user,
+                                         "X-Storage-Pass" => pass })
 
-			yield response if block_given?
+      yield response if block_given?
 
-			case response.code
-			when 200
-				self.storage_url = response.headers["X-Storage-Url"]
-				self.auth_token = response.headers["X-Auth-Token"]
-				true
-			when 401 then false
-			else raise TotalyWrongException
-			end
-		end
+      case response.code
+      when 200
+        self.storage_url = response.headers["X-Storage-Url"]
+        self.auth_token = response.headers["X-Auth-Token"]
+        true
+      when 401 then false
+      else raise TotalyWrongException
+      end
+    end
 
-		def upload(file_path, box_name, destination)
-			raise NotAuthorized if storage_url.nil?
+    def upload(file_path, box_name, destination, &block)
+      raise NotAuthorized if storage_url.nil?
 
-			file = File.new(file_path)
-			content_type = `file --mime-type #{file_path}`.split(": ").last
-			response = HTTParty.put(storage_url+ "/#{box_name}/#{destination}",
-															headers: {
-																"X-Auth-Token" => auth_token,
-																"Content-Type" => content_type,
-																"Content-Length" => file.size.to_s },
-															body: file.read)
+      file = File.new(file_path)
+      content_type = `file --mime-type #{file_path}`.split(": ").last
 
-			yield response if block_given?
+      response = HTTParty.put(storage_url+ "/#{box_name}/#{destination}",
+                              headers: {
+                                "X-Auth-Token" => auth_token,
+                                "Content-Type" => content_type,
+                                "Content-Length" => file.size.to_s },
+                              body: file.read)
 
-			response.code == 201 ? true : false
-		end
+      if response.code == 401 and authorize
+        return upload(file_path,box_name,destination,&block)
+      end
 
-		def delete(box_name, destination)
-			raise NotAuthorized if storage_url.nil?
+      yield response if block_given?
 
-			response = HTTParty.delete(storage_url+ "/#{box_name}/#{destination}",
-																 headers: { "X-Auth-Token" => auth_token })
+      response.code == 201 ? true : false
+    end
 
-			yield response if block_given?
+    def delete(box_name, destination)
+      raise NotAuthorized if storage_url.nil?
 
-			response.code == 204 ? true : false
-		end
-	end
+      response = HTTParty.delete(storage_url+ "/#{box_name}/#{destination}",
+                                 headers: { "X-Auth-Token" => auth_token })
+
+      yield response if block_given?
+
+      response.code == 204 ? true : false
+    end
+  end
 end
--- spec/ucloud_storage_spec.rb
+++ spec/ucloud_storage_spec.rb
@@ -6,138 +6,155 @@ require 'support/vcr'
 require 'yaml'
 
 describe UcloudStorage do
-	context "set user/pass info" do
-		before do
-			UcloudStorage.configure do |config|
-				config.user = "abc@mintshop.com"
-				config.pass = "my_api_key"
-			end
-		end
+  context "set user/pass info" do
+    before do
+      UcloudStorage.configure do |config|
+        config.user = "abc@mintshop.com"
+        config.pass = "my_api_key"
+      end
+    end
 
-		it "can set default user/pass info" do
-			UcloudStorage.user.should == "abc@mintshop.com"
-			UcloudStorage.pass.should == "my_api_key"
-		end
+    it "can set default user/pass info" do
+      UcloudStorage.user.should == "abc@mintshop.com"
+      UcloudStorage.pass.should == "my_api_key"
+    end
 
-		it 'uses default uesr/pass info when initialized' do
-			ucloud = UcloudStorage.new
-			ucloud.user.should == "abc@mintshop.com"
-			ucloud.pass.should == "my_api_key"
-		end
-	end
+    it 'uses default uesr/pass info when initialized' do
+      ucloud = UcloudStorage.new
+      ucloud.user.should == "abc@mintshop.com"
+      ucloud.pass.should == "my_api_key"
+    end
+  end
 
-	let(:valid_ucloud) do
-		file = File.open(File.join(File.dirname(__FILE__), "/support/auth_info.yml"))
-		auth_info = YAML.load(file)
-		ucloud = UcloudStorage.new(user: auth_info["valid_user"]["user"],
-															 pass: auth_info["valid_user"]["pass"])
-	end
+  let(:valid_ucloud) do
+    file = File.open(File.join(File.dirname(__FILE__), "/support/auth_info.yml"))
+    auth_info = YAML.load(file)
+    ucloud = UcloudStorage.new(user: auth_info["valid_user"]["user"],
+                               pass: auth_info["valid_user"]["pass"])
+  end
 
-	let(:invalid_ucloud) do
-		invlaid_ucloud = UcloudStorage.new
-		invlaid_ucloud.user = "invalid_user@mintshop.com"
-		invlaid_ucloud.pass = "please download mintshop"
-		invlaid_ucloud
-	end
+  let(:invalid_ucloud) do
+    invlaid_ucloud = UcloudStorage.new
+    invlaid_ucloud.user = "invalid_user@mintshop.com"
+    invlaid_ucloud.pass = "please download mintshop"
+    invlaid_ucloud
+  end
 
-	describe '#authorize' do
-		it "can authorize with valid user/pass" do
-			VCR.use_cassette("storage/v1/auth") do
-				valid_ucloud.authorize.should == true
-				valid_ucloud.storage_url.should_not be_nil
-				valid_ucloud.auth_token.should_not be_nil
-			end
-		end
+  describe '#authorize' do
+    it "can authorize with valid user/pass" do
+      VCR.use_cassette("storage/v1/auth") do
+        valid_ucloud.authorize.should == true
+        valid_ucloud.storage_url.should_not be_nil
+        valid_ucloud.auth_token.should_not be_nil
+      end
+    end
 
-		it "cannot authorize with invalid user/pass" do
-			VCR.use_cassette("storage/v1/auth_fail") do
-				invalid_ucloud.authorize.should == false
-				invalid_ucloud.storage_url.should be_nil
-				invalid_ucloud.auth_token.should be_nil
-			end
-		end
+    it "cannot authorize with invalid user/pass" do
+      VCR.use_cassette("storage/v1/auth_fail") do
+        invalid_ucloud.authorize.should == false
+        invalid_ucloud.storage_url.should be_nil
+        invalid_ucloud.auth_token.should be_nil
+      end
+    end
 
-		it 'yields response' do
-			VCR.use_cassette("storage/v1/auth") do
-				valid_ucloud.authorize do |response|
-					response.code.should == 200
-				end
-			end
-		end
-	end
+    it 'yields response' do
+      VCR.use_cassette("storage/v1/auth") do
+        valid_ucloud.authorize do |response|
+          response.code.should == 200
+        end
+      end
+    end
+  end
 
-	describe "#upload" do
-		it "can upload a file" do
-			VCR.use_cassette('storage/v1/auth') do
-				valid_ucloud.authorize
-			end
+  describe "#upload" do
+    it "can upload a file" do
+      VCR.use_cassette('storage/v1/auth') do
+        valid_ucloud.authorize
+      end
 
-			file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
-			box = 'dev'
-			destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+      file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
+      box = 'dev'
+      destination = 'cropped_images/'+Pathname(file_path).basename.to_s
 
-			VCR.use_cassette("v1/put_storage_object") do
-				valid_ucloud.upload(file_path, box, destination).should be_true
-			end
-		end
+      VCR.use_cassette("v1/put_storage_object") do
+        valid_ucloud.upload(file_path, box, destination).should be_true
+      end
+    end
 
-		it "should fail to upload with invalid file path" do
-			VCR.use_cassette('storage/v1/auth') do
-				valid_ucloud.authorize
-			end
+    it "should fail to upload with invalid file path" do
+      VCR.use_cassette('storage/v1/auth') do
+        valid_ucloud.authorize
+      end
 
-			file_path = File.join(File.dirname(__FILE__), "/fixtures/no_sample_file.txt")
-			box = 'dev'
-			destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+      file_path = File.join(File.dirname(__FILE__), "/fixtures/no_sample_file.txt")
+      box = 'dev'
+      destination = 'cropped_images/'+Pathname(file_path).basename.to_s
 
-			expect {
-				valid_ucloud.upload(file_path, box, destination)
-			}.to raise_error(Errno::ENOENT)
-		end
+      expect {
+        valid_ucloud.upload(file_path, box, destination)
+      }.to raise_error(Errno::ENOENT)
+    end
 
-		it "should fail to upload without authorization" do
-			file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
-			box = 'dev'
-			destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+    it "should fail to upload without authorization" do
+      file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
+      box = 'dev'
+      destination = 'cropped_images/'+Pathname(file_path).basename.to_s
 
-			expect {
-				valid_ucloud.upload(file_path, box, destination).should be_true
-			}.to raise_error(UcloudStorage::NotAuthorized)
-		end
+      expect {
+        valid_ucloud.upload(file_path, box, destination).should be_true
+      }.to raise_error(UcloudStorage::NotAuthorized)
+    end
 
-		it 'yields response' do
-			VCR.use_cassette('storage/v1/auth') do
-				valid_ucloud.authorize
-			end
+    it "should retry to upload if authorization failure response" do
+      VCR.use_cassette('storage/v1/auth') do
+        valid_ucloud.authorize
+      end
 
-			file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
-			box = 'dev'
-			destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+      file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
+      box = 'dev'
+      destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+      valid_ucloud.auth_token += "a"
 
-			VCR.use_cassette("v1/put_storage_object") do
-				valid_ucloud.upload(file_path, box, destination) do |response|
-					response.code.should == 201
-				end
-			end
-		end
-	end
+      VCR.use_cassette("v1/put_storage_object_with_auth_fail") do
+        valid_ucloud.upload(file_path, box, destination) do |response|
+          response.code.should == 201
+        end
+      end
+    end
 
-	describe "#delete" do
-		it 'should delete updated object' do
-			valid_ucloud.authorize
-			file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
-			box = 'dev_box'
-			destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+    it 'yields response' do
+      VCR.use_cassette('storage/v1/auth') do
+        valid_ucloud.authorize
+      end
 
-			VCR.use_cassette("v1/put_storage_object_02") do
-				valid_ucloud.upload(file_path, box, destination)
-			end
+      file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
+      box = 'dev'
+      destination = 'cropped_images/'+Pathname(file_path).basename.to_s
 
-			VCR.use_cassette("v1/delete_storage_object_02") do
-				valid_ucloud.delete(box, destination) do |response|
-					response.code.should == 204
-				end.should == true
-			end
-		end
-	end
+      VCR.use_cassette("v1/put_storage_object") do
+        valid_ucloud.upload(file_path, box, destination) do |response|
+          response.code.should == 201
+        end
+      end
+    end
+  end
+
+  describe "#delete" do
+    it 'should delete updated object' do
+      valid_ucloud.authorize
+      file_path = File.join(File.dirname(__FILE__), "/fixtures/sample_file.txt")
+      box = 'dev_box'
+      destination = 'cropped_images/'+Pathname(file_path).basename.to_s
+
+      VCR.use_cassette("v1/put_storage_object_02") do
+        valid_ucloud.upload(file_path, box, destination)
+      end
+
+      VCR.use_cassette("v1/delete_storage_object_02") do
+        valid_ucloud.delete(box, destination) do |response|
+          response.code.should == 204
+        end.should == true
+      end
+    end
+  end
 end
